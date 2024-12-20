1c9ad0d6cb21c11be1f43414d38547188beb4904
ClaytonWas <claywasmund@gmail.com>
2024-12-20 01:59:33 -0500

    404 workaround.

--- vite.config.js
+++ vite.config.js
@@ -6,21 +6,4 @@ export default defineConfig({
   plugins: [
     react(),
   ],
-  server: {
-    proxy: {
-      '/old-path': {
-        target: 'https://wasmund.ca', // Your destination URL
-        rewrite: (path) => path.replace(/^\/old-path/, '/new-path'),
-      },
-    },
-    // Manually configure fallback to index.html for routes
-    configureServer(server) {
-      server.middlewares.use((req, res, next) => {
-        if (!req.url.includes('.') && !req.url.startsWith('/api')) {
-          req.url = '/'; // Redirect all non-file requests to the root
-        }
-        next();
-      });
-    },
-  },
 });
